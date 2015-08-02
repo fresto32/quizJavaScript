fa21193425a332d312821086fe8166f41e343c06
Taj Pereira <fresto32@Tajs-MacBook-Pro.local>
2015-08-02 17:15:16 +0930

    finished product

--- quizShow/quiz.js
+++ quizShow/quiz.js
@@ -1,155 +1,95 @@
 $(document).ready(function(){
 
-   $('.question').prepend($('<h2>Here will go the question</h2>'));
-   var score = 0;
-
-
-// Question Constructor Function
-function Question() {
-
-	questionText = '';
-	choiceOne = '';
-	choiceTwo = '';
-	choiceThree = '';
-	choiceFour = '';
-	answer = '';
-}
-
-// Setting up Questions
-var questionOne = new Question ();
-questionOne.questionText = 'How many letters are in the alphabet?';
-questionOne.choiceOne = '2';
-questionOne.choiceTwo = '3';
-questionOne.choiceThree = '7';
-questionOne.choiceFour = '27';
-answer = '4';
-
-
-var questionTwo = new Question ();
-questionTwo.questionText = 'What is my name?';
-questionTwo.choiceOne = 'T';
-questionTwo.choiceTwo = 'A';
-questionTwo.choiceThree = 'J';
-questionTwo.choiceFour = 'Taj';
-answer = '4';
-
-var questionArray = [questionOne,questionTwo];
 
 //Setting up Initial Constraints
-$('.choices').hide();
-$('.next-button').text('Start the Game');
-$('.question').find($('h2')).hide();
-$('.question').find($('p')).hide();
+	var questionDiv = $('.question');
+	var questionLabel = questionDiv.find($('h2'));
+	var choicesDiv = $('.choices');
+	choicesDiv.hide();
+	var choiceOneT = choicesDiv.find($('.choice-one'));
+	var choiceTwoT = choicesDiv.find($('.choice-two'));
+	var choiceThreeT = choicesDiv.find($('.choice-three'));
+	var choiceFourT = choicesDiv.find($('.choice-four'));
 
-var questionCount = 0;
-//Initializing when Game Commences
+
+
+	var nextButton = $('.next-button');
+
+	nextButton.text('Start the Game!');
+	var score = 0;
+	var scoreLabel = $('.score');
+	var questionCount = 0;
+
+
+//Initializing JSON Objects from questionBank
+$.getJSON('questionBank.json', function(response){
+        questionBankArray = response;
+
+			$.each(questionBankArray.questionBank, function(index, item) {
+			    item.htmlFormatter = function(index, item){
+			    	questionLabel.text(this.questionText);
+			    	choiceOneT.text(this.choiceOne);
+			    	choiceTwoT.text(this.choiceTwo);
+			    	choiceThreeT.text(this.choiceThree);
+			    	choiceFourT.text(this.choiceFour);
+
+				};
+		});
+	});
+
 $('.next').on('click','button', function(){
 
-//Question One
+	
+	if(questionCount<questionBankArray.questionBank.length){
+		choicesDiv.show();
+		questionBankArray.questionBank[questionCount].htmlFormatter();
+		nextButton.hide();
+		
+	} else {
 
-if (questionCount===0){
-	$('.next-button').text('Next');
+		questionLabel.text('You finished the test with a score of ' + score);
+		nextButton.hide();
 
-	$('.question').find($('h2')).text(questionOne.questionText);
-	$('.question').find($('h2')).show();
-	$('.question').find($('p')).show();
-
-
-	$('.choice-one').text(questionOne.choiceOne);
-	$('.choice-two').text(questionOne.choiceTwo);
-	$('.choice-three').text(questionOne.choiceThree);
-	$('.choice-four').text(questionOne.choiceFour);
-
-	$('.choices').show();
 	}
 
-console.log(questionCount);
-
-// Question Two
-if (questionCount === 1) {
-
-	$('.next-button').text('Next');
-
-	$('.question').find($('h2')).text(questionTwo.questionText);
-	$('.question').find($('h2')).show();
-	$('.question').find($('p')).show();
-
-
-	$('.choice-one').text(questionTwo.choiceOne);
-	$('.choice-two').text(questionTwo.choiceTwo);
-	$('.choice-three').text(questionTwo.choiceThree);
-	$('.choice-four').text(questionTwo.choiceFour);
-
-	$('.choices').show();
-
-
-
-}
-
-// Final
-if (questionCount === 2) {
-
-	$('.next-button').hide();
-
-	$('.question').find($('h2')).text('Congratulations on Finishing the Test');
-	$('.question').find($('h2')).show();
-	$('.question').find($('p')).show();
-
-
-
-}
-
 });
-
 
 $('.choices').on('click','button', function(){
 
+	if ($(this).hasClass(questionBankArray.questionBank[questionCount].answer)) {
 
-	if (questionCount === 1){
+		questionLabel.text('Correct!!');
+		score++;
 
-		if ($(this).hasClass('choice-four')){
+	} else {
 
-			$('.question').find($('h2')).text('Correct!');
-			score++;
-			$('.score').text('Score: ' + score+ '');
+		questionLabel.text('Wow... Youre stupid');
 
-		} else {
-
-			$('.question').find($('h2')).text('Incorrect!');
-			$('.score').text('Score: ' + score+ '');
-
-		}
-
-		questionCount++;
-
-		$('.choices').hide();
-
-		
 	}
 
-		if (questionCount === 0){
+questionCount++;
 
-		if ($(this).hasClass('choice-four')){
+nextButton.show().text('Next Question');
 
-			$('.question').find($('h2')).text('Correct!');
-			score++;
-			$('.score').text('Score: ' + score+ '');
-
-		} else {
-
-			$('.question').find($('h2')).text('Incorrect!');
-			$('.score').text('Score: ' + score+ '');
-
-		}
-
-		questionCount++;
-
-		$('.choices').hide();
-
+	if(questionCount===questionBankArray.questionBank.length-1){
 		
+		nextButton.text('Final Question');
+	
+	} else if(questionCount===questionBankArray.questionBank.length) {
+
+		nextButton.text('Results');
+
+	}
+
+scoreLabel.text('Score: '+score);
+
+choicesDiv.hide();
 
 
-	}	
+});
+
+
+
 
 
 
@@ -158,5 +98,11 @@ $('.choices').on('click','button', function(){
 
 
 
-});
+
+
+
+
+
+
+
 
